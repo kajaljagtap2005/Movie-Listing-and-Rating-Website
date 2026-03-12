3542edd589ee38e595687e5d18ef6a99a3084e72
kajaljagtap2005 <kajaljagtapofficial@gmail.com>
2026-03-12 21:44:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
+const movies = [
+    {
+        id: 1,
+        title: "Lagaan",
+        genre: "Drama, Sport",
+        year: 2001,
+        poster: "https://m.media-amazon.com/images/I/61U2mIts0pL.jpg",
+        desc: "In 1890s India, an arrogant British army captain challenges the residents of Champaner to a game of cricket.",
+        trailer: "https://youtu.be/pZuowUaZEqA?si=umb1aBxq3IDPdyFy",
+        ratings: [5, 4, 5]
+    },
+    {
+        id: 2,
+        title: "Dangal",
+        genre: "Biography, Sport",
+        year: 2016,
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSD8zskwDQjc9mJVikd9W-ImYP56U7CIQRwZg&s",
+        desc: "Former wrestler Mahavir Singh Phogat trains his daughters for the Commonwealth Games.",
+        trailer: "https://youtu.be/XuIVa_K59QE?si=yKu2slNa9yIj5K_U",
+        ratings: [5, 5, 5]
+    },
+    {
+        id: 3,
+        title: "Jolly LLB 2",
+        genre: "Comedy, Drama",
+        year: 2013,
+        poster: "https://m.media-amazon.com/images/M/MV5BYjcwODQ4ZDgtODQyZS00MmE1LWE4ZjQtZjQ5MWQzYTMyNDIwXkEyXkFqcGc@._V1_.jpg",
+        desc: "A small-time lawyer attempts to earn a name for himself by taking on a high-profile hit-and-run case.",
+        trailer: "https://youtu.be/q07SQFmL4rM?si=Revvc2MIiAjhhILV",
+        ratings: [4, 3, 4]
+    },
+    {
+        id: 4,
+        title: "Maa",
+        genre: "Horror, Drama",
+        year: 1991,
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2Sul51fce3z_FbvageYhYG1NEKzZpbTKb-A&s",
+        desc: "A tragic story of a mother who returns as a spirit to protect her child.",
+        trailer: "https://youtu.be/zwtZj6YB9xk?si=IwCtsK1owfq1OxGv",
+        ratings: [4, 5]
+    },
+    {
+        id: 5,
+        title: "English Vinglish",
+        genre: "Comedy, Drama",
+        year: 2012,
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSQxY77QSlbpQvy9uWHpd8n-x_aZJhTCT5oA&s",
+        desc: "A quiet housewife discovers her self-worth after enrolling in an English-speaking course.",
+        trailer: "https://youtu.be/wmGVY4T88dc?si=_t8skQY6AYiuxdOa",
+        ratings: [5, 5, 4]
+    }
+];
 
